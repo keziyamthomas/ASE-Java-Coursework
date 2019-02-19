56aaf0924a98e0f77c976c9f18813e09db5415dd
keziyamthomas <46924807+keziyamthomas@users.noreply.github.com>
2019-02-19 08:51:47 +0400

    Add files via upload
    Custom Exception added

---
+++ PatternException.java
@@ -0,0 +1,12 @@
+
+
+//Reference - https://www.youtube.com/watch?v=Q-66i9EVR_8 
+
+public class PatternException extends Exception {
+	
+	public PatternException(String message) {
+		super(message);
+	}
+	
+
+}
