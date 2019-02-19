d0a21aa2b2c491ecea49ccbbb39930bf79008e1d
Minu <Minu@LAPTOP-DO7O1TRR>
2019-02-19 16:46:14 +0530

    Updated Order Class

--- Order.java
+++ Order.java
@@ -1,4 +1,33 @@
+import java.sql.Timestamp;
 
 public class Order {
+	private Timestamp time;
+	private String custId;
+	private Item item;
+	private int quantity;
+	public Timestamp getTime() {
+		return time;
+	}
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
 }
