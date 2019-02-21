2c11539fc2f7674c399aced256ba7222664ce93f
Minu99 <45223458+Minu99@users.noreply.github.com>
2019-02-21 23:50:41 +0400

    Add files via upload
    Changes made in Test

--- processClassTest.java
+++ processClassTest.java
@@ -2,33 +2,55 @@ package coffeeshopapp;
 
 import static org.junit.Assert.*;
 
+
+
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
 public class processClassTest {
+	static ProcessClass processClass;
+	static String item1="Falooda";
+	static String item2="Pasta";
+	static String item3="Coffee";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		processClass=new ProcessClass();
+		
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
+	
+	@Test()
 	public final void test() {
-		fail("Not yet implemented"); // TODO
+		double out=25.0;
+		processClass.readItems();
+		Assert. assertEquals(out,processClass.getUnitPriceByItemName(item1));
+		
+		
 	}
+	@Test
+	public final void test2() {
+		double out=30.0;
+		processClass.readItems();
+		Assert.assertEquals(out,processClass.getUnitPriceByItemName(item2));
+				
+	}
+	@Test
+	public final void test3() {
+		double out=20.0;
+		processClass.readItems();
+		Assert.assertEquals(out,processClass.getUnitPriceByItemName(item3));
+				
+	}
+	
+	
+
+
+	
 
 }
